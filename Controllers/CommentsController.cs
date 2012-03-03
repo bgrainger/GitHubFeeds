ae822f47ce80d87ccf9c89641cad0b34fbdad9ae
Bradley Grainger <bgrainger@gmail.com>
2012-03-03 09:39:20 -0800

    Add CommentsController.

---
+++ Controllers/CommentsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace GitHubCommentsFeed.Controllers
+{
+    public class CommentsController : Controller
+    {
+        public ActionResult List(string server, string user, string repo)
+        {
+        	return Content("server = " + server + ", user = " + user + ", repo = " + repo);
+        }
+    }
+}
--- Global.asax.cs
+++ Global.asax.cs
@@ -16,8 +16,8 @@ namespace GitHubCommentsFeed
 
 			routes.MapRoute(
 				"Default", // Route name
-				"{controller}/{action}/{id}", // URL with parameters
-				new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+				"comments/{server}/{user}/{repo}",
+				new { controller = "Comments", action = "List" }
 			);
 
 		}
