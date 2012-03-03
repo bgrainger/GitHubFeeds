02d60dd23f47caa5bff383abc38353c8f2e259d8
Bradley Grainger <bgrainger@gmail.com>
2012-03-03 09:27:14 -0800

    Add empty ASP.NET MVC project.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GitHubCommentsFeed")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bradley Grainger")]
+[assembly: AssemblyProduct("GitHubCommentsFeed")]
+[assembly: AssemblyCopyright("Copyright 2012 Bradley Grainger")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
